4f4c7106e36543b17e03ae590d3b35df110abbe2
Marcelo Núñez <luciacasti@awesomeapp.com>
2025-01-14 19:21:04 +0100

    imprime cambio de Marcelo

--- MiClase1.java
+++ MiClase1.java
@@ -7,6 +7,7 @@ public class MiClase1
             System.out.println("12 * "+ i + " = " + 12 * i + "\n");
 			System.out.println("¡Hola, mundo!");
 			System.out.println("¿Cómo estás?");
+			System.out.println("Cambio de Marcelo");
         }
     }
 }
